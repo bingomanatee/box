86f599bbf44cbe7d13c3334686bd4751563de03e
David Edelhart <dave@wonderlandlabs.com>
2014-09-26 11:57:47 -0700

    got 2d voxels working

--- Assets/Poly/Gen.cs
+++ Assets/Poly/Gen.cs
@@ -17,6 +17,16 @@ namespace Poly
 				public int squareCount;
 				public byte[,] blocks;
 				int[] vertOrder = new int[]{0, 1, 3, 1, 2, 3};
+				const int Y_DIM = 50;
+				int X_DIM = 50;
+
+				void ClearMeshData ()
+				{
+		
+						newVertices = new List<Vector3> ();
+						newTriangles = new List<int> ();
+						newUV = new List<Vector2> ();
+				}
 
 #region colliders
 				public List<Vector3> colVertices = new List<Vector3> ();
@@ -32,17 +42,24 @@ namespace Poly
 						colCount++;
 				}
 
+				void ClearColliderData ()
+				{
+						Debug.Log ("ClearColliderData");
+						colVertices.Clear ();
+						colTriangles.Clear ();
+						colCount = 0;
+						squareCount = 0;
+				}
+
 				void InitCollMesh ()
 				{
+			
+						Debug.Log ("InitCollMesh");
 						Mesh newMesh = new Mesh ();
 			
 						newMesh.vertices = colVertices.ToArray ();
 						newMesh.triangles = colTriangles.ToArray ();
 						col.sharedMesh = newMesh;
-			
-						colVertices.Clear ();
-						colTriangles.Clear ();
-						colCount = 0;
 				}
 
 				void GenCollider (int x, int y)
@@ -62,7 +79,7 @@ namespace Poly
 			
 						MakeCollTriangles ();
 			
-						  //bot
+						//bot
 						colVertices.Add (new Vector3 (x, y - 1, 0));
 						colVertices.Add (new Vector3 (x + 1, y - 1, 0));
 						colVertices.Add (new Vector3 (x + 1, y - 1, 1));
@@ -92,7 +109,21 @@ namespace Poly
 
 		#region generators
 
-				// Use this for initialization
+				void UpdateMesh ()
+				{
+						Debug.Log ("UpdateMesh: with " + newVertices.Count + " verts");
+						mesh.Clear ();
+						mesh.vertices = newVertices.ToArray ();
+						mesh.triangles = newTriangles.ToArray ();
+						mesh.uv = newUV.ToArray (); // add this line to the code here
+						mesh.Optimize ();
+						mesh.RecalculateNormals ();
+			
+				}
+		
+/**
+builds up the vertices and UV definition for the mesh
+*/
 				void GenSquare (float x, float y, Vector2 textureSpace)
 				{
 						mesh = GetComponent<MeshFilter> ().mesh;
@@ -115,11 +146,14 @@ namespace Poly
 			
 				}
 
+/**
+ * convert bit data into mesh 
+*/
 				void BuildMesh ()
 				{
-						InitCollMesh ();
-						for (int px=0; px<blocks.GetLength(0); px++) {
-								for (int py=0; py<blocks.GetLength(1); py++) {
+						Debug.Log ("Build Mesh");
+						for (int px=0; px<X_DIM; px++) {
+								for (int py=0; py< Y_DIM; py++) {
 										if (blocks [px, py] != 0) { // if !air
 						
 												// GenCollider here, this will apply it
@@ -135,44 +169,56 @@ namespace Poly
 								}
 						}
 				}
-				// Update is called once per frame
-				void UpdateMesh ()
-				{
-						mesh.Clear ();
-						mesh.vertices = newVertices.ToArray ();
-						mesh.triangles = newTriangles.ToArray ();
-						mesh.uv = newUV.ToArray (); // add this line to the code here
-						mesh.Optimize ();
-						mesh.RecalculateNormals ();
 
-						squareCount = 0;
-						newVertices.Clear ();
-						newTriangles.Clear ();
-						newUV.Clear ();
-
-			InitCollMesh();
-				}
-
+				/**
+* sets the byte definition of the ground
+*/
 				void GenTerrain ()
 				{
-						blocks = new byte[10, 10];
+						blocks = new byte[X_DIM, Y_DIM];
 			
 						for (int px=0; px<blocks.GetLength(0); px++) {
+								int stone = Noise (px, 0, 80, 15, 1);
+								stone += Noise (px, 0, 50, 30, 1);
+								stone += Noise (px, 0, 10, 10, 1);
+								stone += 75;
+				
+								int dirt = Noise (px, 0, 100f, 35, 1);
+								dirt += Noise (px, 100, 50, 30, 1);
+								dirt += 75;
+				
+				
 								for (int py=0; py<blocks.GetLength(1); py++) {
-										if (py == groundLevel) {
-												blocks [px, py] = 2;
-										} else if (py < groundLevel) {
+										if (py < stone) {
 												blocks [px, py] = 1;
+						
+												//The next three lines make dirt spots in random places
+												if (Noise (px, py, 12, 16, 1) > 10) {
+														blocks [px, py] = 2;
+							
+												}
+						
+												//The next three lines remove dirt and rock to make caves in certain places
+												if (Noise (px, py * 2, 16, 14, 1) > 10) { //Caves
+														blocks [px, py] = 0;
+							
+												}
+						
+										} else if (py < dirt) {
+												blocks [px, py] = 2;
 										}
+					
+					
 								}
 						}
+			
 				}
 		
 		#endregion
 
 		#region loop
 
-		
+				int decTime = 0;
 				int groundLevel = 5;
 
 				void Start ()
@@ -182,19 +228,40 @@ namespace Poly
 
 				void Update ()
 				{
-						GenTerrain ();
-						BuildMesh ();
+						GenTerrain (); // byte definition
+						InitCollMesh (); // collision mesh def
+						BuildMesh (); // byte data to face data
 						UpdateMesh ();
-						if (Random.value > 0.5)
-								++ groundLevel;
-						else
-								-- groundLevel;
-
-						if (groundLevel != Mathf.Clamp (groundLevel, 0, 10))
-								groundLevel = 0;
+						MoveGround ();
+						ClearMeshData ();
+						ClearColliderData ();
 				}
 
+				void MoveGround ()
+				{
+						int d = (int)(Time.time * 10);
+						if (decTime != d) {
+								if (Random.value > 0.5)
+										++ groundLevel;
+								else
+										-- groundLevel;
+								decTime = d;
+								if (groundLevel != Mathf.Clamp (groundLevel, 0, 10))
+										groundLevel = 0;
+						}
+				}
 
 		#endregion
+
+#region noise
+
+				int Noise (int x, int y, float scale, float mag, float exp)
+				{
+			
+						return (int)(Mathf.Pow ((Mathf.PerlinNoise (x / scale, y / scale) * mag), exp)); 
+			
+				}
+
+#endregion
 		}
 }
